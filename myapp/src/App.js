2d5d94a73c12709160f9d5babaa2ab06f918ca99
Gankhulug <huluguu0202@gmail.com>
2023-01-08 20:48:56 +0800

    asd

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "../src/style.scss";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Container>
+      <div>
+        <Button>click</Button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          asdfghjfdsxgfdsgfdsgfdsfgdsgfdsfgdsgfdsdffddsxgfdsgfdsgfdsfgdsgfdsfgdsgfdsdffddsxgfdsgfdsgfdsfgdsgfdsfgdsgfdsdffddsxgfdsgfdsgfdsfgdsgfdsfgdsgfdsdffddsxgfdsgfdsgfdsfgdsgfdsfgdsgfdsdffddsxgfdsgfdsgfdsfgdsgfdsfgdsgfdsdffddsxgfdsgfdsgfdsfgdsgfdsfgdsgfdsdffddsxgfdsgfdsgfdsfgdsgfdsfgdsgfdsdffddsxgfdsgfdsgfdsfgdsgfdsfgdsgfdsdffdsgfdsgfdsgfdsgfdsgfdsgfdsgfdsgfdsgfdsgfdsgfdsgfdsgfdsgfdsgfdsgfdsgfdsgfdssgfdsgfdsgfdsgfdsgfdsgfdsgfdsgfdsgfdsgfdsgfdsgfdsg
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </Container>
   );
 }
 
