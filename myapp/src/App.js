4d7d8c20ee1cefc17978ca6c965134919e93518b
Gankhulug02 <huluguu0202@gmail.com>
2023-01-10 13:28:53 +0800

    w

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -9,44 +9,44 @@ import Button from "react-bootstrap/esm/Button";
 import Btns from "./Components/index";
 import { useState } from "react";
 
-const items = [
-  {
-    title: "Add",
-    variant: "danger",
-  },
-  {
-    title: "Minus",
-    variant: "primary",
-  },
-];
+// const items = [
+//   {
+//     title: "Add",
+//     variant: "danger",
+//   },
+//   {
+//     title: "Minus",
+//     variant: "primary",
+//   },
+// ];
 
 function App() {
-  const [count, setCount] = useState(0);
+  //   const [count, setCount] = useState(0);
 
-  const handleClick = (title) => {
-    if (title === "Add") {
-      setCount(count + 1);
-      console.log("asd");
-    } else {
-      setCount(count - 1);
-      console.log("asd");
-    }
-  };
+  //   const handleClick = (title) => {
+  //     if (title === "Add") {
+  //       setCount(count + 1);
+  //       console.log("asd");
+  //     } else {
+  //       setCount(count - 1);
+  //       console.log("asd");
+  //     }
+  //   };
 
-  // return <Home />;
-  return (
-    <div>
-      <h1>Count: {count}</h1>
+  return <Home />;
+  //   return (
+  //     <div>
+  //       <h1>Count: {count}</h1>
 
-      {items.map((item) => (
-        <Btns
-          title={item.title}
-          variant={item.variant}
-          handleClick={handleClick}
-        />
-      ))}
-    </div>
-  );
+  //       {items.map((item) => (
+  //         <Btns
+  //           title={item.title}
+  //           variant={item.variant}
+  //           handleClick={handleClick}
+  //         />
+  //       ))}
+  //     </div>
+  //   );
 }
 
 export default App;
